b7bbbf2ae00aa4e0d6c9f7fd11d3515f814685a9
Nicholas26-design <52610537+Nicholas26-design@users.noreply.github.com>
2023-05-14 14:49:58 -0400

    Update Dictionary Attack Program.py

--- PorfolioProjects-main/Dictionary Attack Program.py
+++ PorfolioProjects-main/Dictionary Attack Program.py
@@ -7,28 +7,30 @@ chars = string.printable
 chars_list = list(chars)
 password = pyautogui.password("Enter a password: ")
 guess_password = ""  # stores guessed passwords
-Common_Passwords = open('Password Dictionary.txt', 'r+')
+Common_Passwords = open('10k-most-common.txt', 'r+')
+
+
+def brute_force():
+    guess_password = ""  # stores guessed passwords
+
+    while (guess_password != password):
+        guess_password = random.choices(chars_list, k=len(password))
+
+        print("<==================" + str(guess_password) + "==================>")
+
+        if (guess_password == list(password)):
+            print("Success in brute forcing the following password: " + "".join(guess_password))
+            break
+
 
 # read file content
 readfile = Common_Passwords.read()
 
 # checking condition for string found or not
 if password in readfile:
-	print('The password is ', password, '')
+    print('The password is in a dictionary and is: ',password, '')
 else:
-	print("default to brute force")
-
-#Block of code for brute force attacks. Need to make into fuction
-# while(guess_password != password):
-#     guess_password = random.choices(chars_list, k=len(password))
-#
-#     print("<==================" + str(guess_password) + "==================>")
-#
-#     if(guess_password == list(password)):
-#         print("Your password is: " + "".join(guess_password))
-#         break
+    brute_force()
 
 # closing a file
 Common_Passwords.close()
-
-
