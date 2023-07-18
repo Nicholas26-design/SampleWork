99f856b668012d25950525e49a1d467a56ec761a
Nicholas26-design <52610537+Nicholas26-design@users.noreply.github.com>
2023-07-18 12:11:15 -0400

    Add files via upload

---
+++ PorfolioProjects-reference_starter/Basic Level/Passcodes.py
@@ -0,0 +1,22 @@
+# A real world application of this code would be a password on iphone.
+# With that example as well, you'd want to have a guess limit
+
+secret_word = "password"
+guess = ""  # variable to store the guess
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter Guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of guesses! You Lose!")
+else:
+    print("You win!")
+
+
